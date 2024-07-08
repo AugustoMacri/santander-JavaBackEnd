3ce51b5ce751e90b9904d45141b610ce628f13c1
AugustoMacri <augusto.macri@hotmail.com>
2024-07-08 10:48:06 -0300

    Autenticação com Banco de Dados

--- Spring Framework/dio-spring-security/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
+++ Spring Framework/dio-spring-security/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
@@ -1,36 +1,49 @@
 package dio.dio_spring_security;
 
+import dio.dio_spring_security.config.SecurityDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 
     //Aqui o objetivo é permitir criar mais de um usuário e perfis de acesso, tivemos que substituir a websecurityadapter por uma abordagem mais nova
     //usando assim segurityFilterChain e UserDetailService
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated()
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .requestMatchers("/managers").hasAnyRole("MANAGERS")
+                        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                        .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults());
         return http.build();
     }
-
+    /*
     @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsManager = new InMemoryUserDetailsManager();
@@ -50,4 +63,6 @@ public class WebSecurityConfig {
 
         return userDetailsManager;
     }
+    */
+
 }
